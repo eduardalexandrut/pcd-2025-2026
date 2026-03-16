da5bdd968538234e758d6ec22446a03ee18a20a6
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-03-16 14:09:12 +0100

    Implemented FakeLatch

--- lab-activities/lab-activity-05/pcd-lab-05/src/main/java/pcd/lab05/monitors/ex_latch/FakeLatch.java
+++ lab-activities/lab-activity-05/pcd-lab-05/src/main/java/pcd/lab05/monitors/ex_latch/FakeLatch.java
@@ -4,14 +4,28 @@ package pcd.lab05.monitors.ex_latch;
  * Latch - to be implemented
  */
 public class FakeLatch implements Latch {
-	
-	public FakeLatch() {}
+	private int count;
+	public FakeLatch(final int count) {
+        this.count = Math.max(count,0);
+    }
 	
 	@Override
-	public void await() throws InterruptedException {}
+	public synchronized void await() throws InterruptedException {
+        while (count > 0) {
+            wait();
+        }
+    }
 
 	@Override
-	public void countDown() {}
+	public synchronized void countDown() {
+        if (count > 0) {
+            this.count--;
+
+            if (this.count == 0){
+                notifyAll();
+            }
+        }
+    }
 
 	
 }
