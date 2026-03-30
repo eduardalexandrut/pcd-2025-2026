2e006498d860482c359443312f8756d12e65cf8b
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-03-30 18:11:39 +0200

    feat: Define Controller interface

---
+++ assignments/assignment-01/src/main/java/pcd/poool/controller/Controller.java
@@ -0,0 +1,27 @@
+package pcd.poool.controller;
+
+import pcd.poool.model.Ball;
+import pcd.poool.model.BallState;
+import pcd.poool.model.P2d;
+
+import java.util.List;
+
+public interface Controller {
+
+    void processInput(String key);
+
+    /**
+     * @return the current state of the User's ball.
+     */
+    BallState getUserBallState();
+
+    /**
+     * @return the current state of the NPC's ball.
+     */
+    BallState getNPCBallState();
+
+    /**
+     * @return a snapshot DTO of all the balls' state.
+     */
+    List<BallState> getStateSnapshot();
+}
