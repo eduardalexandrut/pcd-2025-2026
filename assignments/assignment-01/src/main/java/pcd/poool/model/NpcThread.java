aeec4622a5717f240f47a8b7e3ec5575b64fd377
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-04-04 20:06:12 +0200

    feat: Implement NpcThread

---
+++ assignments/assignment-01/src/main/java/pcd/poool/model/NpcThread.java
@@ -0,0 +1,22 @@
+package pcd.poool.model;
+
+public class NpcThread extends Thread {
+    private final Ball npcBall;
+
+    public NpcThread(Ball npcBall) {
+        this.npcBall = npcBall;
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            final V2d newVel = new V2d(Math.random() * 10, Math.random() * 10);
+
+            this.npcBall.kick(newVel);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) { break; }
+        }
+    }
+}
