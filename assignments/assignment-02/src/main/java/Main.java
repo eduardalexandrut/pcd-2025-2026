c729243626bb49e2c1eb993b9927f7566a9e5e6c
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-04-14 23:59:18 +0200

    refactor: Rename main java class

--- assignments/assignment-02/src/main/java/Main.java
+++ assignments/assignment-02/src/main/java/Main.java
@@ -3,7 +3,7 @@ import view.View;
 
 import javax.swing.*;
 
-public class FSStatLib {
+public class Main {
     public static void main(String[] args) {
         final Controller controller = new Controller();
 
